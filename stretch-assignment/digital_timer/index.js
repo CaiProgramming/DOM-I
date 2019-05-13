d7720e656c348139eccec50f0ac4f39340c8b1eb
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-13 16:44:33 -0400

    Finished Stretch

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,24 @@
+let count = 0;
+let counter = setInterval(alertFunc, 10);
+
+function alertFunc() {
+  console.log((count += 1));
+  let char = count
+    .toString()
+    .split("")
+    .reverse();
+  let ms = document.querySelector("#msTens");
+  let msh = document.querySelector("#msHundreds");
+  let so = document.querySelector("#secondOnes");
+  let st = document.querySelector("#secondTens");
+  console.log(char);
+  ms.textContent = char[0];
+  if (char[1]) msh.textContent = char[1];
+  if (char[2]) so.textContent = char[2];
+  if (char[3]) st.textContent = char[3];
+
+  console.log("hello");
+  if (count > 1000 || count === 1000) {
+    window.clearInterval(counter);
+  }
+}
